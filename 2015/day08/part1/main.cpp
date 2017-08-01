8a05acb3cc568cb6ca162969ffc7ebf7b2e99433
Gergő Nyitrai <ger.nyitrai@gmail.com>
2017-08-01 18:27:31 +0200

    added solutions for 2015/day08 puzzles

---
+++ 2015/day08/part1/main.cpp
@@ -0,0 +1,58 @@
+/* --- Day 8: Matchsticks ---
+ * Space on the sleigh is limited this year, and so Santa will be bringing his
+ * list as a digital copy. He needs to know how much space it will take up
+ * when stored.
+ * It is common in many programming languages to provide a way to escape
+ * special characters in strings. For example, C, JavaScript, Perl, Python,
+ * and even PHP handle special characters in very similar ways.
+ * However, it is important to realize the difference between the number of
+ * characters in the code representation of the string literal and the number of
+ * characters in the in-memory string itself.
+ * For example:
+ * - "" is 2 characters of code (the two double quotes), but the string
+ *   contains zero characters.
+ * - "abc" is 5 characters of code, but 3 characters in the string data.
+ * - "aaa\"aaa" is 10 characters of code, but the string itself contains
+ *   six "a" characters and a single, escaped quote character, for a total
+ *   of 7 characters in the string data.
+ * - "\x27" is 6 characters of code, but the string itself contains just
+ *   one - an apostrophe ('), escaped using hexadecimal notation.
+ * Santa's list is a file that contains many double-quoted string literals,
+ * one on each line. The only escape sequences used are \\ (which represents a
+ * single backslash), \" (which represents a lone double-quote character), and
+ * \x plus two hexadecimal characters (which represents a single character
+ * with that ASCII code).
+ * Disregarding the whitespace in the file, what is the number of characters
+ * of code for string literals minus the number of characters in memory for
+ * the values of the strings in total for the entire file?
+ * For example, given the four strings above, the total number of characters
+ * of string code (2 + 5 + 10 + 6 = 23) minus the total number of characters
+ * in memory for string values (0 + 3 + 7 + 1 = 11) is 23 - 11 = 12.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main () {
+	int result = 0;
+	std::string line;
+	std::ifstream ifs("./day08/input.txt", std::ifstream::binary);
+	if (!ifs.is_open()) {
+		std::cout << "Can't open file";
+		return 1;
+	}
+	while (std::getline(ifs, line)) {
+		result += 2;
+		for (int i = 1; i < line.size() - 2; i++)
+			if (line[i] == '\\') {
+				if (line[i + 1] == '\\' || line[i + 1] == '\"')
+					result++;
+				if (line[i + 1] == 'x')
+					result += 3;
+				i++;
+			}
+	}
+	std::cout << result;
+	return 0;
+}
