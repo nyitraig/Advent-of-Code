5253f3e2e1ca7867687cd1cf634a865616a2f291
nyitraig <nyitrai@protonmail.com>
2018-02-08 22:45:38 +0100

    refactored day04 solutions

---
+++ 2017/day04/part1/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var n = 0;
+
+document.getElementsByTagName("pre")[0].innerHTML.trim().split('\n').forEach(e => {
+	var arr = e.split(' ').sort();
+	for (var i = 0; i < arr.length - 1; i++) {
+		if (arr[i] == arr[i + 1]) {
+			n--;
+			break;
+		}
+	}
+	n++;
+});
+
+console.log(n);
