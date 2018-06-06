96ea1405695aab1a2c897834082b96cdad60383c
Gergo Nyitrai <nyitrai@protonmail.com>
2018-06-06 23:00:52 +0200

    refactor commit

--- 2016/day03/part2/script.rb
+++ 2016/day03/part2/script.rb
@@ -1,3 +1,3 @@
 puts File.readlines("input").map { |line| line.split.map(&:to_i) }
-  .each_slice(3).flat_map { |x, y, z | x.zip(y, z) }
+  .each_slice(3).flat_map { |x, y, z| x.zip(y, z) }
   .count { |a, b, c| a + b > c && a + c > b && b + c > a }
