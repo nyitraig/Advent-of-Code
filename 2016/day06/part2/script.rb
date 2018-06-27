2bf3c6252503a7978f1c7e2773ba5acaf441f8b6
Gergo Nyitrai <nyitrai@protonmail.com>
2018-06-27 17:34:38 +0200

    refactor commit

--- 2016/day06/part2/script.rb
+++ 2016/day06/part2/script.rb
@@ -1,2 +1,2 @@
-puts File.readlines("input").map { |x| x.strip.chars }.reduce(&:zip)
-  .map { |x| x.flatten.group_by(&:dup).values.sort_by(&:size).first[0] }.join
+puts File.readlines("input").map { |x| x.strip.chars }.transpose
+  .map { |x| x.group_by(&:dup).values.sort_by(&:size).first[0] }.join
