c1e6b50fd56838339b9fa78fdd8c50d90256b962
Gergő Nyitrai <nyitraig+github@protonmail.com>
2019-12-25 22:53:26 +0100

    add 2019/day01/part2

---
+++ 2019/day01/part2/script.py
@@ -0,0 +1,8 @@
+import sys
+
+def fuel(mass):
+    f = int(mass / 3) - 2
+    return f + fuel(f) if f > 0 else 0
+
+input = [int(x) for x in open(sys.argv[1]).read().strip().split('\n')]
+print(sum([fuel(x) for x in input]))
