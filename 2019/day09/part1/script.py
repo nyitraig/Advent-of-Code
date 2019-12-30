29c2ffe047572d2fba917488c16b3e3e942231c6
Gergő Nyitrai <nyitraig+github@protonmail.com>
2019-12-30 18:56:12 +0100

    adjust previous solutions to use new intcode computer

--- 2019/day09/part1/script.py
+++ 2019/day09/part1/script.py
@@ -5,6 +5,6 @@ from intcode import Computer
 input = [int(x) for x in open(sys.argv[1]).read().strip().split(',')]
 
 computer = Computer(input)
-computer.input = [1]
+computer.provide_input(1)
 computer.run()
 print(computer.output)
--- 2019/day09/part2/script.py
+++ 2019/day09/part2/script.py
@@ -5,6 +5,6 @@ from intcode import Computer
 input = [int(x) for x in open(sys.argv[1]).read().strip().split(',')]
 
 computer = Computer(input)
-computer.input = [2]
+computer.provide_input(2)
 computer.run()
 print(computer.output)
