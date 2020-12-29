9c5b6d70ec30cb0054d95282170fd2094bf663c5
nyitraig <nyitraig+github@protonmail.com>
2020-12-29 23:41:55 +0100

    add 2020/day03/

---
+++ 2020/day03/part1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadAllLines(inputFilePath);
+            var result = 0;
+            for (int i = 0, j = 0; i < input.Count(); i++, j = (j + 3) % input[0].Count())
+            {
+                if (input[i][j] == '#')
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 2020/day03/part2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static int CountTrees(string[] map, int right, int down)
+        {
+            var count = 0;
+            for (int i = 0, j = 0; i < map.Count(); i += down, j = (j + right) % map[0].Count())
+            {
+                if (map[i][j] == '#')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadAllLines(inputFilePath);
+            Console.WriteLine((long)CountTrees(input, 1, 1) *
+                              CountTrees(input, 3, 1) *
+                              CountTrees(input, 5, 1) *
+                              CountTrees(input, 7, 1) *
+                              CountTrees(input, 1, 2));
+        }
+    }
+}
