5e8c4a9e8d8c8353e06ee95bb2c83680b27d11e7
nyitraig <nyitraig+github@protonmail.com>
2020-12-29 19:04:15 +0100

    add 2020/day02/

---
+++ 2020/day02/part1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadAllLines(inputFilePath);
+            var regex = new Regex(@"^(?<min>\d+)-(?<max>\d+)\s(?<character>[a-z]):\s(?<password>[a-z]+)$");
+            var result = 0;
+            foreach (var line in input)
+            {
+                var match = regex.Match(line);
+                var minCount = int.Parse(match.Groups["min"].Value);
+                var maxCount = int.Parse(match.Groups["max"].Value);
+                var character = char.Parse(match.Groups["character"].Value);
+                var password = match.Groups["password"].Value;
+                var characterCount = password.Count(c => c == character);
+                if (characterCount >= minCount && characterCount <= maxCount)
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 2020/day02/part2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadAllLines(inputFilePath);
+            var regex = new Regex(@"^(?<i>\d+)-(?<j>\d+)\s(?<character>[a-z]):\s(?<password>[a-z]+)$");
+            var result = 0;
+            foreach (var line in input)
+            {
+                var match = regex.Match(line);
+                var i = int.Parse(match.Groups["i"].Value) - 1;
+                var j = int.Parse(match.Groups["j"].Value) - 1;
+                var character = char.Parse(match.Groups["character"].Value);
+                var password = match.Groups["password"].Value;
+                if ((password[i] == character && password[j] != character) ||
+                    (password[i] != character && password[j] == character))
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
