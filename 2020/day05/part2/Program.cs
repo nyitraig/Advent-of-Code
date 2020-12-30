9761fbc3f584b89561eda26395d62f76bb8bc13a
nyitraig <nyitraig+github@protonmail.com>
2020-12-30 11:19:12 +0100

    add 2020/day05/part2/

---
+++ 2020/day05/part2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static int GetSeatId(string pass)
+        {
+            var row = Convert.ToInt32(pass[0..7].Replace('F', '0').Replace('B', '1'), 2);
+            var column = Convert.ToInt32(pass[7..].Replace('L', '0').Replace('R', '1'), 2);
+            return row * 8 + column;
+        }
+
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadLines(inputFilePath).Select(GetSeatId).ToList();
+            var result = input.Where(x => !input.Contains(x + 1) && input.Contains(x + 2)).First() + 1;
+            Console.WriteLine(result);
+        }
+    }
+}
