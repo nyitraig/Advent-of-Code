d4de6a932002e46db289341f4fce1a696ad9f68a
nyitraig <nyitraig+github@protonmail.com>
2020-12-31 15:20:33 +0100

    add 2020/day07/

---
+++ 2020/day07/part1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static HashSet<string> GetContainers(string[] rules, string color)
+        {
+            var containers = rules.Where(x => x.Contains(color) && !x.StartsWith(color)).Select(x => x.Split(" bags")[0]);
+            var result = new HashSet<string>(containers);
+            foreach (var container in containers)
+            {
+                result.UnionWith(GetContainers(rules, container));
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadAllLines(inputFilePath);
+            Console.WriteLine(GetContainers(input, "shiny gold").Count);
+        }
+    }
+}
---
+++ 2020/day07/part2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static int CountContents(string[] rules, string color)
+        {
+            var rule = rules.First(x => x.StartsWith(color));
+            var regex = new Regex(@"(?<amount>\d+) (?<color>[a-z]+ [a-z]+) bags?");
+            var matches = regex.Matches(rule);
+            var result = 0;
+            foreach (Match match in matches)
+            {
+                result += int.Parse(match.Groups["amount"].Value) * (CountContents(rules, match.Groups["color"].Value) + 1);
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadAllLines(inputFilePath);
+            Console.WriteLine(CountContents(input, "shiny gold"));
+        }
+    }
+}
