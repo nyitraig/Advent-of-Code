513db28e692ff897a1bf26b29a342e7cd523b180
nyitraig <nyitraig+github@protonmail.com>
2021-01-01 18:23:11 +0100

    add 2020/day08/

---
+++ 2020/day08/part1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadLines(inputFilePath).Select(x =>
+            {
+                var instruction = x.Split();
+                var operation = instruction[0];
+                var argument = int.Parse(instruction[1]);
+                return (operation, argument, executed: false);
+            }).ToList();
+            var accumulator = 0;
+            var i = 0;
+            while (true)
+            {
+                var (operation, argument, executed) = input[i];
+                if (executed)
+                {
+                    break;
+                }
+                input[i] = (operation, argument, executed: true);
+                if (operation == "acc")
+                {
+                    accumulator += argument;
+                    i += 1;
+                }
+                if (operation == "jmp")
+                {
+                    i += argument;
+                }
+                if (operation == "nop")
+                {
+                    i += 1;
+                }
+            }
+            Console.WriteLine(accumulator);
+        }
+    }
+}
---
+++ 2020/day08/part2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static bool Run(List<(string operation, int argument, bool executed)> program, out int accumulator)
+        {
+            accumulator = 0;
+            for (int i = 0; i < program.Count;)
+            {
+                var (operation, argument, executed) = program[i];
+                if (executed)
+                {
+                    return false;
+                }
+                program[i] = (operation, argument, executed: true);
+                if (operation == "acc")
+                {
+                    accumulator += argument;
+                    i += 1;
+                }
+                if (operation == "jmp")
+                {
+                    i += argument;
+                }
+                if (operation == "nop")
+                {
+                    i += 1;
+                }
+            }
+            return true;
+        }
+        static int Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadLines(inputFilePath).Select(x =>
+            {
+                var instruction = x.Split();
+                var operation = instruction[0];
+                var argument = int.Parse(instruction[1]);
+                return (operation, argument, executed: false);
+            }).ToList();
+            for (int i = 0; i < input.Count; i++)
+            {
+                var (operation, argument, executed) = input[i];
+                if (operation == "jmp" || operation == "nop")
+                {
+                    var program = new List<(string operation, int argument, bool executed)>(input)
+                    {
+                        [i] = (operation == "jmp" ? "nop" : "jmp", argument, executed)
+                    };
+                    if (Run(program, out int accumulator))
+                    {
+                        Console.WriteLine(accumulator);
+                        return 0;
+                    }
+                }
+            }
+            return 1;
+        }
+    }
+}
