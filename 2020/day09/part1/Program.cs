d279b652cf0e4a333bd74263dea69d3642243994
nyitraig <nyitraig+github@protonmail.com>
2021-01-01 20:42:55 +0100

    add 2020/day09/

---
+++ 2020/day09/part1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadLines(inputFilePath).Select(long.Parse).ToList();
+            for (int i = 25; i < input.Count; i++)
+            {
+                var num = input[i];
+                var slice = input.GetRange(i - 25, 25);
+                if (!slice.Any(x => slice.Contains(num - x)))
+                {
+                    Console.WriteLine(num);
+                    return 0;
+                }
+            }
+            return 1;
+        }
+    }
+}
---
+++ 2020/day09/part2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadLines(inputFilePath).Select(long.Parse).ToList();
+            long invalidNumber = -1; // the solution for part 1
+            for (int i = 25; i < input.Count; i++)
+            {
+                var num = input[i];
+                var slice = input.GetRange(i - 25, 25);
+                if (!slice.Any(x => slice.Contains(num - x)))
+                {
+                    invalidNumber = num;
+                    break;
+                }
+            }
+            for (int i = 0; i < input.Count; i++)
+            {
+                var j = 2;
+                while (true)
+                {
+                    var sum = input.Skip(i).Take(j).Sum();
+                    if (sum == invalidNumber)
+                    {
+                        Console.WriteLine(input[i] + input[i + j - 1]);
+                        return 0;
+                    }
+                    if (sum > invalidNumber)
+                    {
+                        break;
+                    }
+                    j++;
+                }
+            }
+            return 1;
+        }
+    }
+}
