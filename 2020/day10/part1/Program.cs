a924da798ace68d989fbce8ac6cc8d540de589a0
nyitraig <nyitraig+github@protonmail.com>
2021-01-02 18:45:15 +0100

    add 2020/day10/part1/

---
+++ 2020/day10/part1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var input = File.ReadLines(inputFilePath).Select(int.Parse).Append(0).ToList();
+            input.Sort();
+            var differences = input.Take(input.Count - 1).Select((v, i) => input[i + 1] - v);
+            Console.WriteLine(differences.Count(x => x == 1) * (differences.Count(x => x == 3) + 1));
+        }
+    }
+}
